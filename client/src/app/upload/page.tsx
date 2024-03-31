9e5d2e258c45ac505a9d8ad370c184265d71a7a3
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2024-03-31 01:26:26 -0400

    mongodb

--- client/src/app/upload/page.tsx
+++ client/src/app/upload/page.tsx
@@ -1,34 +1,50 @@
 'use client'
 
 import React from 'react'
-import { useState, ChangeEvent,useEffect } from 'react'
+import { useState, ChangeEvent, useEffect } from 'react'
 
 const Upload = () => {
   let setup = false;
   const [file, setFile] = useState<File | null>(null);
   const [label, setLabel] = useState<File | null>(null);
-  const [name, setName] = useState<string>('');
-  const [labelDetails, setLabelDetails] = useState<string>('');
+
+  const [itemDetails, setItemDetails] = useState([{clothing_name: "", color: "", image_url: ""}]);
+  const [labelDetails, setLabelDetails] = useState({});
+  
+  // const [itemName, setItemName] = useState<string>("");
+  // const [itemColor, setItemColor] = useState<string>("");
+  // const [itemImage, setItemImage] = useState<string>("");
+
+  // const [materialName, setMaterialName] = useState<string>("");
+  // const [materialPercent, setMaterialPercent] = useState<string>("");
+  // const [materialRank, setMaterialRank] = useState<string>("");
+  // const [materialDesc, setMaterialDesc] = useState<string>("");
+  // const [materialImage, setMaterialImage] = useState<string>("");
+  const [user, setUser] = useState({});
+
   const [coins, setCoins] = useState<string>("-1");
+
+
+
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const selected = e.target.files && e.target.files[0];
     setFile(selected);
-  
+
     if (selected) {
       try {
         const reader = new FileReader();
         reader.readAsDataURL(selected);
         reader.onloadend = async () => {
-          const base64Data = reader.result?.toString().split(',')[1]; 
-          
+          const base64Data = reader.result?.toString().split(',')[1];
+
           const s3_res = await fetch('https://jeiq0otl56.execute-api.us-east-2.amazonaws.com/default/slo-images', {
             method: 'POST',
             body: JSON.stringify({ image: base64Data })
           });
           
           const s3_data = await s3_res.json();
-  
+
           const res = await fetch('http://localhost:5000/vision1', {
             method: 'POST',
             headers: {
@@ -36,9 +52,9 @@ const Upload = () => {
             },
             body: JSON.stringify({ id: s3_data.id })
           });
-  
+
           const data = await res.json();
-          setName(data[0].clothing_name);
+          setItemDetails(data);
         };
       } catch (err) {
         console.log(err);
@@ -49,21 +65,21 @@ const Upload = () => {
   const handleChangeLabel = async (e: ChangeEvent<HTMLInputElement>) => {
     const selected = e.target.files && e.target.files[0];
     setLabel(selected);
-  
+
     if (selected) {
       try {
         const reader = new FileReader();
         reader.readAsDataURL(selected);
         reader.onloadend = async () => {
-          const base64Data = reader.result?.toString().split(',')[1]; 
-          
+          const base64Data = reader.result?.toString().split(',')[1];
+
           const s3_res = await fetch('https://jeiq0otl56.execute-api.us-east-2.amazonaws.com/default/slo-images', {
             method: 'POST',
             body: JSON.stringify({ image: base64Data })
           });
           
           const s3_data = await s3_res.json();
-  
+
           const res = await fetch('http://localhost:5000/vision2', {
             method: 'POST',
             headers: {
@@ -71,9 +87,9 @@ const Upload = () => {
             },
             body: JSON.stringify({ id: s3_data.id })
           });
-  
+
           const data = await res.json();
-          setLabelDetails(JSON.stringify(data));
+          setLabelDetails(data);
         };
       } catch (err) {
         console.log(err);
@@ -81,7 +97,22 @@ const Upload = () => {
     }
   };
 
-  const handleSubmit = async () => {
+  const handleSubmitClothes = async () => {
+    if (!itemDetails || !labelDetails) return
+    const formData = new FormData();
+    formData.append('email', 'example@mail.com'); // Replace with actual email
+    formData.append('name', itemDetails[0].clothing_name);
+    formData.append('color', itemDetails[0].color);
+    formData.append('image', itemDetails[0].image_url);
+    formData.append('label', JSON.stringify(labelDetails));
+
+    const res = await fetch('http://localhost:5000/add_clothes', {
+      method: 'POST',
+      body: formData
+    });
+  }
+
+  const handleSubmitUser = async () => {
     if (!file) return
     const formData = new FormData()
   }
@@ -89,7 +120,9 @@ const Upload = () => {
 
 
   useEffect(() => {
-  }, [name, labelDetails]);
+    
+
+  }, [itemDetails, labelDetails]);
 
   useEffect(() => {
     if (setup === true) return
@@ -107,8 +140,7 @@ const Upload = () => {
   }, []);
 
 
-  console.log(coins)
-  if (coins === "-1" ) {
+  if (coins === "-1") {
     return (<></>)
   }
   else return (
@@ -116,13 +148,17 @@ const Upload = () => {
 
       <div>
         <h1>My wallet has {coins} coins</h1>
-        <input type="file" onChange={handleChange} />
-        {file && <img width={100} src={URL.createObjectURL(file)} alt="preview" />}
-        <h1>{name}</h1>
-        <input type="file" onChange={handleChangeLabel} />
-        {label && <img width={100} src={URL.createObjectURL(label)} alt="preview" />}
-        <h3>{labelDetails}</h3>
-        <button onClick={handleSubmit}>Upload</button>
+
+
+        <form onSubmit={e => e.preventDefault()}>
+          <input type="file" onChange={handleChange} />
+          {file && <img width={100} src={URL.createObjectURL(file)} alt="preview" />}
+          <h1>{JSON.stringify(itemDetails)}</h1>
+          <input type="file" onChange={handleChangeLabel} />
+          {label && <img width={100} src={URL.createObjectURL(label)} alt="preview" />}
+          <h3>{JSON.stringify(labelDetails)}</h3>
+          <button onClick={handleSubmitClothes}>Upload</button>
+        </form>
       </div>
 
     </main>
